be7cc1a594825b6b94af7066689789e4009f1fec
Christopher Finke <cfinke@gmail.com>
2013-06-11 22:04:31 +0000

    Confirming minimum WP requirements and fixing erroneous jquery.spin requirement.

--- inline-preview.php
+++ inline-preview.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Inline Preview
 Plugin URI: http://www.chrisfinke.com/wordpress/plugins/inline-preview
@@ -7,14 +6,13 @@ Description: Adds a post preview next to the editor instead of in a new tab.
 Version: 1.0
 Author: Christopher Finke
 Author URI: http://www.chrisfinke.com/
-License: GPLv2
 */
 
 function inline_preview_enqueue( $hook ) {
 	if ( 'post-new.php' != $hook && 'post.php' != $hook )
 		return;
 
-	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), array( 'jquery', 'jquery.spin' ), true );
+	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', true );
 	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ) );
 }
 
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Inline Preview ===
 Contributors: cfinke
 Tags: preview, live preview
-Requires at least: 
+Requires at least: 3.4
 Tested up to: 3.6
 Stable tag: 1.0
 License: GPLv2 or later
