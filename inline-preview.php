868a5ba8911dbee2c8b13db5e6ab7a80a9a04c0e
Christopher Finke <cfinke@gmail.com>
2013-06-12 03:47:25 +0000

    Version 1.1

--- inline-preview.php
+++ inline-preview.php
@@ -3,7 +3,7 @@
 Plugin Name: Inline Preview
 Plugin URI: http://www.chrisfinke.com/wordpress/plugins/inline-preview
 Description: Adds a post preview next to the editor instead of in a new tab.
-Version: 1.0
+Version: 1.1
 Author: Christopher Finke
 Author URI: http://www.chrisfinke.com/
 */
