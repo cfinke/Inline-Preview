f4750accfbe60348696d0ecdf42e2de8d6987650
Christopher Finke <cfinke@gmail.com>
2013-06-12 14:18:39 +0000

    Reducing code level of most of the click handler.

--- inline-preview/inline-preview.js
+++ inline-preview/inline-preview.js
@@ -2,86 +2,87 @@ jQuery( function ( $ ) {
 	var animationDuration = 500;
 
 	$( '#post-preview' ).on( 'click', function () {
-		if ( $( '#inline-preview-container' ).length == 0 ) {
-			var windowWidth = $( document ).width();
-			
-			// Editor width in percent.
-			var editorWidth = 60;
-			
-			if ( localStorage['inline-preview-width'] )
-				editorWidth = 100 - localStorage['inline-preview-width'];
-			
-			var editorWidthPx = Math.round( windowWidth * ( editorWidth / 100 ) );
-			var previewWidthPx = Math.round( windowWidth * ( ( 100 - editorWidth ) / 100 ) );
+		if ( $( '#inline-preview-container' ).length > 0 )
+			return;
+		
+		var windowWidth = $( document ).width();
+		
+		// Editor width in percent.
+		var editorWidth = 60;
+		
+		if ( localStorage['inline-preview-width'] )
+			editorWidth = 100 - localStorage['inline-preview-width'];
+		
+		var editorWidthPx = Math.round( windowWidth * ( editorWidth / 100 ) );
+		var previewWidthPx = Math.round( windowWidth * ( ( 100 - editorWidth ) / 100 ) );
 
-			$( 'body' ).addClass( 'inline-preview' );
-			
-			var editorContainer = $( '#wpwrap' );
-			var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' ).css( 'width', previewWidthPx ).css( 'left', windowWidth );
-			
-			if ( $( '#post-body' ).hasClass( 'columns-2' ) ) {
-				var postBodyClass = 'columns-2';
-				$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' );
-			}
-			else {
-				var postBodyClass = 'columns-1';
-			}
-
-			editorContainer
-				.before( previewContainer )
-				.animate(
-					{ 'padding-right' : previewWidthPx },
-					{ duration : animationDuration, queue : false }
-				);
-			
-			previewContainer
-				.animate(
-					{ 'left' : editorWidthPx },
-					{ duration : animationDuration, queue : false, complete : function () {
-						previewContainer
-							.append(
-								$( '<a id="inline-preview-close">X</a>' )
-									.on( 'click', function ( e ) {
-										e.preventDefault();
-										
-										$( this ).hide();
-
-										previewContainer
-											.animate(
-												{ 'left' : $( window ).width() },
-												{ duration : animationDuration, queue : false, complete : function () {
-													$( '#post-body' ).removeClass( 'columns-1' ).addClass( postBodyClass );
-
-													previewContainer.remove();
-													$( 'body' ).removeClass( 'inline-preview' );
-												} }
-											);
-										editorContainer.animate(
-											{ 'padding-right' : '0' },
-											{ duration : animationDuration, queue : false }
-										);
-									} )
-							)
-							.css( 'z-index', '1000' );
-					} }
-				);
-			
-			previewContainer.resizable( {
-				minWidth : 100,
-				handles : "w",
-				start : function () {
-					previewContainer.css( 'z-index', '' );
-				},
-				resize : function ( event, ui ) {
-					var totalWidth = previewContainer.parent().width();
-					editorContainer.css( 'padding-right', ui.size.width );
-				},
-				stop : function ( event, ui ) {
-					var totalWidth = previewContainer.parent().width();
-					localStorage['inline-preview-width'] = Math.round( ui.size.width / totalWidth * 100 );
-					previewContainer.css( 'z-index', '1000' );
-				}
-			} );
+		$( 'body' ).addClass( 'inline-preview' );
+		
+		var editorContainer = $( '#wpwrap' );
+		var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' ).css( 'width', previewWidthPx ).css( 'left', windowWidth );
+		
+		if ( $( '#post-body' ).hasClass( 'columns-2' ) ) {
+			var postBodyClass = 'columns-2';
+			$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' );
 		}
+		else {
+			var postBodyClass = 'columns-1';
+		}
+
+		editorContainer
+			.before( previewContainer )
+			.animate(
+				{ 'padding-right' : previewWidthPx },
+				{ duration : animationDuration, queue : false }
+			);
+		
+		previewContainer
+			.animate(
+				{ 'left' : editorWidthPx },
+				{ duration : animationDuration, queue : false, complete : function () {
+					previewContainer
+						.append(
+							$( '<a id="inline-preview-close">X</a>' )
+								.on( 'click', function ( e ) {
+									e.preventDefault();
+									
+									$( this ).hide();
+
+									previewContainer
+										.animate(
+											{ 'left' : $( window ).width() },
+											{ duration : animationDuration, queue : false, complete : function () {
+												$( '#post-body' ).removeClass( 'columns-1' ).addClass( postBodyClass );
+
+												previewContainer.remove();
+												$( 'body' ).removeClass( 'inline-preview' );
+											} }
+										);
+									editorContainer.animate(
+										{ 'padding-right' : '0' },
+										{ duration : animationDuration, queue : false }
+									);
+								} )
+						)
+						.css( 'z-index', '1000' );
+				} }
+			);
+		
+		previewContainer.resizable( {
+			minWidth : 100,
+			handles : "w",
+			start : function () {
+				previewContainer.css( 'z-index', '' );
+			},
+			resize : function ( event, ui ) {
+				var totalWidth = previewContainer.parent().width();
+				editorContainer.css( 'padding-right', ui.size.width );
+			},
+			stop : function ( event, ui ) {
+				var totalWidth = previewContainer.parent().width();
+				localStorage['inline-preview-width'] = Math.round( ui.size.width / totalWidth * 100 );
+				previewContainer.css( 'z-index', '1000' );
+			}
+		} );
 	} );
 } );
