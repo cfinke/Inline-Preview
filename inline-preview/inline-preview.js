61599774c797ad0ff9e11907696784b9bea31247
Christopher Finke <cfinke@gmail.com>
2013-06-12 05:12:44 +0000

    New method for sliding the preview frame in.
    Set the full width of the frame and then move it in using positioning (and padding on the editor container) instead of animating the width.  It's a smoother animation, and it doesn't reflow the iframe contents as it moves.
    
    Also makes the preview resizable, and the width is persistent.

--- inline-preview/inline-preview.js
+++ inline-preview/inline-preview.js
@@ -1,13 +1,21 @@
 jQuery( function ( $ ) {
 	$( '#post-preview' ).on( 'click', function () {
 		if ( $( '#inline-preview-container' ).length == 0 ) {
+			var windowWidth = $( document ).width();
+			
 			// Editor width in percent.
 			var editorWidth = 60;
 			
+			if ( localStorage['inline-preview-width'] )
+				editorWidth = 100 - localStorage['inline-preview-width'];
+			
+			var editorWidthPx = Math.round( windowWidth * ( editorWidth / 100 ) );
+			var previewWidthPx = Math.round( windowWidth * ( ( 100 - editorWidth ) / 100 ) );
+
 			$( 'body' ).addClass( 'inline-preview' );
 			
 			var editorContainer = $( '#wpwrap' );
-			var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' );
+			var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' ).css( 'width', previewWidthPx ).css( 'left', windowWidth );
 			
 			if ( $( '#post-body' ).hasClass( 'columns-2' ) )
 				$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' ).data( 'inline-preview-modified', true );
@@ -15,13 +23,13 @@ jQuery( function ( $ ) {
 			editorContainer
 				.before( previewContainer )
 				.animate(
-					{ 'width' : editorWidth + '%' },
+					{ 'padding-right' : previewWidthPx },
 					{ duration : 1000, queue : false }
 				);
 			
 			previewContainer
 				.animate(
-					{ 'width' : ( 100 - editorWidth ) + '%' },
+					{ 'left' : editorWidthPx },
 					{ duration : 1000, queue : false, complete : function () {
 						previewContainer
 							.append(
@@ -33,7 +41,7 @@ jQuery( function ( $ ) {
 
 										previewContainer
 											.animate(
-												{ 'width' : '0' },
+												{ 'left' : $( window ).width() },
 												{ duration : 1000, queue : false, complete : function () {
 													if ( $( '#post-body' ).data( 'inline-preview-modified' ) )
 														$( '#post-body' ).removeClass( 'columns-1' ).addClass( 'columns-2' ).data( 'inline-preview-columns', null );
@@ -43,13 +51,31 @@ jQuery( function ( $ ) {
 												} }
 											);
 										editorContainer.animate(
-											{ 'width' : '100%' },
+											{ 'padding-right' : '0' },
 											{ duration : 1000, queue : false }
 										);
 									} )
 							)
+							.css( 'z-index', '1000' );
 					} }
 				);
+			
+			previewContainer.resizable( {
+				minWidth : 100,
+				handles : "w",
+				start : function () {
+					previewContainer.css( 'z-index', '' );
+				},
+				resize : function ( event, ui ) {
+					var totalWidth = previewContainer.parent().width();
+					editorContainer.css( 'padding-right', ui.size.width );
+				},
+				stop : function ( event, ui ) {
+					var totalWidth = previewContainer.parent().width();
+					localStorage['inline-preview-width'] = Math.round( ui.size.width / totalWidth * 100 );
+					previewContainer.css( 'z-index', '1000' );
+				}
+			} );
 		}
 	} );
 } );
