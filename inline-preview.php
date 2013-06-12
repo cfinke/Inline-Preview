0798d76496caa46fc310a05ffccad2b2ccd4507a
Christopher Finke <cfinke@gmail.com>
2013-06-12 21:22:52 +0000

    Version 2.

--- inline-preview.php
+++ inline-preview.php
@@ -12,8 +12,8 @@ function inline_preview_enqueue( $hook ) {
 	if ( 'post-new.php' != $hook && 'post.php' != $hook )
 		return;
 
-	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', true );
-	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ) );
+	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', '2' );
+	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '2' );
 }
 
 add_action( 'admin_enqueue_scripts', 'inline_preview_enqueue' );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: cfinke
 Tags: preview, live preview
 Requires at least: 3.4
 Tested up to: 3.6
-Stable tag: 1.0
+Stable tag: 2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
