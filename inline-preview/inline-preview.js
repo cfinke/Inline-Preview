0a2decc1c3602333bf8203e1c482b12a762d2780
Christopher Finke <cfinke@gmail.com>
2013-06-12 03:50:37 +0000

    Added a close button to the preview iframe.

--- inline-preview/inline-preview.js
+++ inline-preview/inline-preview.js
@@ -1,9 +1,55 @@
 jQuery( function ( $ ) {
 	$( '#post-preview' ).on( 'click', function () {
 		if ( $( '#inline-preview-container' ).length == 0 ) {
+			// Editor width in percent.
+			var editorWidth = 60;
+			
 			$( 'body' ).addClass( 'inline-preview' );
-			$( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' ).insertBefore( '#wpwrap' );
-			$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' );
+			
+			var editorContainer = $( '#wpwrap' );
+			var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview"></iframe></div>' );
+			
+			if ( $( '#post-body' ).hasClass( 'columns-2' ) )
+				$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' ).data( 'inline-preview-modified', true );
+
+			editorContainer
+				.before( previewContainer )
+				.animate(
+					{ 'width' : editorWidth + '%' },
+					{ duration : 1000, queue : false }
+				);
+			
+			previewContainer
+				.animate(
+					{ 'width' : ( 100 - editorWidth ) + '%' },
+					{ duration : 1000, queue : false, complete : function () {
+						previewContainer
+							.append(
+								$( '<a id="inline-preview-close">X</a>' )
+									.on( 'click', function ( e ) {
+										e.preventDefault();
+										
+										$( this ).hide();
+
+										previewContainer
+											.animate(
+												{ 'width' : '0' },
+												{ duration : 1000, queue : false, complete : function () {
+													if ( $( '#post-body' ).data( 'inline-preview-modified' ) )
+														$( '#post-body' ).removeClass( 'columns-1' ).addClass( 'columns-2' ).data( 'inline-preview-columns', null );
+
+													previewContainer.remove();
+													$( 'body' ).removeClass( 'inline-preview' );
+												} }
+											);
+										editorContainer.animate(
+											{ 'width' : '100%' },
+											{ duration : 1000, queue : false }
+										);
+									} )
+							)
+					} }
+				);
 		}
 	} );
 } );
