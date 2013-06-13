68907eae26b5df6384820f6f4870919bdbc4613e
Christopher Finke <cfinke@gmail.com>
2013-06-13 16:34:40 +0000

    Version 3b1.

--- inline-preview.php
+++ inline-preview.php
@@ -3,7 +3,7 @@
 Plugin Name: Inline Preview
 Plugin URI: http://www.chrisfinke.com/wordpress/plugins/inline-preview
 Description: Adds a post preview next to the editor instead of in a new tab.
-Version: 2b1
+Version: 3b1
 Author: Christopher Finke
 Author URI: http://www.chrisfinke.com/
 */
@@ -12,8 +12,8 @@ function inline_preview_enqueue( $hook ) {
 	if ( 'post-new.php' != $hook && 'post.php' != $hook )
 		return;
 
-	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', '2' );
-	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '2' );
+	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), 'jquery', '3b1' );
+	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '3b1' );
 }
 
 add_action( 'admin_enqueue_scripts', 'inline_preview_enqueue' );
