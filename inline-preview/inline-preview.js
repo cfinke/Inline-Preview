e7a0a37ee247c85a7358d1800c34f8e372dee96e
Christopher Finke <cfinke@gmail.com>
2013-06-17 05:34:12 +0000

    Capture clicks on preview links in the header message after the user saves a draft.

--- inline-preview/inline-preview.js
+++ inline-preview/inline-preview.js
@@ -15,87 +15,102 @@ jQuery( function ( $ ) {
 			var self = this;
 
 			$( '#post-preview' ).on( 'click.inline-preview', function () {
-				if ( $( '#inline-preview-container' ).length > 0 )
-					return;
-
-				var windowWidth = $( document ).width();
-
-				// Editor width in percent.
-				var editorWidth = 60;
-
-				if ( localStorage['inline-preview-width'] )
-					editorWidth = 100 - localStorage['inline-preview-width'];
-
-				var editorWidthPx = Math.round( windowWidth * ( editorWidth / 100 ) );
-				var previewWidthPx = Math.round( windowWidth * ( ( 100 - editorWidth ) / 100 ) );
-
-				$( 'body' ).addClass( 'inline-preview' );
-
-				var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview" id="inline-preview-iframe""></iframe></div>' ).css( 'width', previewWidthPx ).css( 'left', windowWidth );
-
-				if ( $( '#post-body' ).hasClass( 'columns-2' ) ) {
-					self.postBodyClass = 'columns-2';
-					$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' );
-				}
-				else {
-					self.postBodyClass = 'columns-1';
-				}
-
-				self.editorContainer
-					.before( previewContainer )
-					.animate(
-						{ 'padding-right' : previewWidthPx },
-						{ duration : self.animationDuration, queue : false }
-					);
-
-				previewContainer
-					.addClass( 'loading' )
-					.animate(
-						{ 'left' : editorWidthPx },
-						{ duration : self.animationDuration, queue : false, complete : function () {
-							previewContainer
-								.append(
-									$( '<a id="inline-preview-close">X</a>' )
-										.on( 'click.inline-preview', function ( e ) {
-											e.preventDefault();
-											self.remove();
-										} )
-								)
-								.css( 'z-index', '1000' );
-						} }
-					);
-
-				previewContainer.resizable( {
-					minWidth : 100,
-					handles : "w",
-					start : function () {
-						previewContainer.css( 'z-index', '' );
-					},
-					resize : function ( event, ui ) {
-						var totalWidth = previewContainer.parent().width();
-						self.editorContainer.css( 'padding-right', ui.size.width );
-					},
-					stop : function ( event, ui ) {
-						var totalWidth = previewContainer.parent().width();
-						localStorage['inline-preview-width'] = Math.round( ui.size.width / totalWidth * 100 );
-						previewContainer.css( 'z-index', '1000' );
-					}
-				} );
-
-				$( '#inline-preview-iframe' ).on( 'load.inline-preview', function () {
-					self.userIsActive = false;
-					previewContainer.removeClass( 'loading' );
-					$( this ).unbind( 'load.inline-preview' ).removeAttr( 'name' );
-					$( '#inline-preview-container' ).append( $( '<iframe name="wp-preview" id="inline-preview-hidden-iframe"></iframe>' ) );
-				} );
-
-				// Don't continually refresh the iframe if nothing is happening.
-				$( document ).on( 'keydown.inline-preview click.inline-preview', function () {
-					self.userIsActive = true;
-				} );
-
-				self.timer = setTimeout( function () { self.reload(); }, self.interval );
+				self.preview();
 			} );
+			
+			$( 'a[id!="post-preview"][href$="preview=true"]' ).on( 'click.inline-preview', function ( e ) {
+				e.preventDefault();
+				
+				$( '#post-preview' ).click();
+			} );
+		},
+		
+		/**
+		 * The user has intiated a preview.
+		 */
+		preview : function () {
+			var self = this;
+			
+			if ( $( '#inline-preview-container' ).length > 0 )
+				return;
+
+			var windowWidth = $( document ).width();
+
+			// Editor width in percent.
+			var editorWidth = 60;
+
+			if ( localStorage['inline-preview-width'] )
+				editorWidth = 100 - localStorage['inline-preview-width'];
+
+			var editorWidthPx = Math.round( windowWidth * ( editorWidth / 100 ) );
+			var previewWidthPx = Math.round( windowWidth * ( ( 100 - editorWidth ) / 100 ) );
+
+			$( 'body' ).addClass( 'inline-preview' );
+
+			var previewContainer = $( '<div id="inline-preview-container"><iframe name="wp-preview" id="inline-preview-iframe""></iframe></div>' ).css( 'width', previewWidthPx ).css( 'left', windowWidth );
+
+			if ( $( '#post-body' ).hasClass( 'columns-2' ) ) {
+				self.postBodyClass = 'columns-2';
+				$( '#post-body' ).removeClass( 'columns-2' ).addClass( 'columns-1' );
+			}
+			else {
+				self.postBodyClass = 'columns-1';
+			}
+
+			self.editorContainer
+				.before( previewContainer )
+				.animate(
+					{ 'padding-right' : previewWidthPx },
+					{ duration : self.animationDuration, queue : false }
+				);
+
+			previewContainer
+				.addClass( 'loading' )
+				.animate(
+					{ 'left' : editorWidthPx },
+					{ duration : self.animationDuration, queue : false, complete : function () {
+						previewContainer
+							.append(
+								$( '<a id="inline-preview-close">X</a>' )
+									.on( 'click.inline-preview', function ( e ) {
+										e.preventDefault();
+										self.remove();
+									} )
+							)
+							.css( 'z-index', '1000' );
+					} }
+				);
+
+			previewContainer.resizable( {
+				minWidth : 100,
+				handles : "w",
+				start : function () {
+					previewContainer.css( 'z-index', '' );
+				},
+				resize : function ( event, ui ) {
+					var totalWidth = previewContainer.parent().width();
+					self.editorContainer.css( 'padding-right', ui.size.width );
+				},
+				stop : function ( event, ui ) {
+					var totalWidth = previewContainer.parent().width();
+					localStorage['inline-preview-width'] = Math.round( ui.size.width / totalWidth * 100 );
+					previewContainer.css( 'z-index', '1000' );
+				}
+			} );
+
+			$( '#inline-preview-iframe' ).on( 'load.inline-preview', function () {
+				self.userIsActive = false;
+				previewContainer.removeClass( 'loading' );
+				$( this ).unbind( 'load.inline-preview' ).removeAttr( 'name' );
+				$( '#inline-preview-container' ).append( $( '<iframe name="wp-preview" id="inline-preview-hidden-iframe"></iframe>' ) );
+			} );
+
+			// Don't continually refresh the iframe if nothing is happening.
+			$( document ).on( 'keydown.inline-preview click.inline-preview', function () {
+				self.userIsActive = true;
+			} );
+
+			self.timer = setTimeout( function () { self.reload(); }, self.interval );
 		},
 		
 		/**
