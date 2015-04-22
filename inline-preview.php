f756d2e060d06cf98841a98e1afc057b8d445d06
Christopher Finke <cfinke@gmail.com>
2015-04-22 18:19:00 +0000

    Reinstate resizability by including styles for resizing.
    See https://core.trac.wordpress.org/ticket/32049

--- inline-preview.php
+++ inline-preview.php
@@ -3,7 +3,7 @@
 Plugin Name: Inline Preview
 Plugin URI: http://www.chrisfinke.com/wordpress/plugins/inline-preview
 Description: Adds a post preview next to the editor instead of in a new tab.
-Version: 4.2
+Version: 4.3b1
 Author: Christopher Finke
 Author URI: http://www.chrisfinke.com/
 */
@@ -12,8 +12,9 @@ function inline_preview_enqueue( $hook ) {
 	if ( 'post-new.php' != $hook && 'post.php' != $hook )
 		return;
 
-	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), array( 'jquery', 'jquery-ui-resizable' ), '4.2' );
-	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '4.2' );
+	wp_enqueue_script( 'inline-preview', plugins_url( 'inline-preview/inline-preview.js', __FILE__ ), array( 'jquery', 'jquery-ui-resizable' ), '4.3b1' );
+	wp_enqueue_style( 'inline-preview-resizable', plugins_url( 'inline-preview/resizable.css', __FILE__ ), array(), '4.3b1' );
+	wp_enqueue_style( 'inline-preview', plugins_url( 'inline-preview/inline-preview.css', __FILE__ ), array(), '4.3b1' );
 	wp_localize_script( 'inline-preview', 'Inline_Preview_Strings', array( 'close' => __( 'Close', 'inline-preview' ) ) );
 }
 
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === Inline Preview ===
 Contributors: cfinke
 Tags: preview, live preview
-Requires at least: 3.7
-Tested up to: 3.9
+Requires at least: 4.1
+Tested up to: 4.1.2
 Stable tag: 4.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
